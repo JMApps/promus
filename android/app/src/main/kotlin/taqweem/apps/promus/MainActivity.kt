7d2270df75d2fcacf97202219051fd78ac166386
JMApps <jmappsmuslim@gmail.com>
2026-07-26 14:34:58 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/taqweem/apps/promus/MainActivity.kt
@@ -0,0 +1,5 @@
+package taqweem.apps.promus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'features/main/pages/root_page.dart';
+
+void main() {
+  runApp(
+    const RootPage(),
+  );
+}
